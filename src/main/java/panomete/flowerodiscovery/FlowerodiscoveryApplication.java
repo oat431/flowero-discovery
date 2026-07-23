33c97676240168630faf44a17b3b5b42e3bb7d67
oat431 <oat431@gmail.com>
2026-07-23 23:24:40 +0700

    feat: init project

---
+++ src/main/java/panomete/flowerodiscovery/FlowerodiscoveryApplication.java
@@ -0,0 +1,13 @@
+package panomete.flowerodiscovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlowerodiscoveryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlowerodiscoveryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/panomete/flowerodiscovery/FlowerodiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package panomete.flowerodiscovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlowerodiscoveryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
